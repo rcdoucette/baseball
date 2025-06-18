6a1f9a187dfe387ccb2ea136f02a8e35a874c70b
rcdoucette <ryan@familusa.org>
2025-06-18 14:30:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,3 +26,8 @@ def index():
 
 if __name__ == "__main__":
     app.run()
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
